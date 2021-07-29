e6d4b270dcaa623d270eafa9646bbdab3782e70b
David Rodriguez <david_rodriguezsuarez@hotmail.com>
2021-07-28 20:46:58 -0600

    Inicial

---
+++ ContosoPizza/Controllers/PizzaController.cs
@@ -0,0 +1,38 @@
+﻿using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoPizza.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() =>
+        PizzaService.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
+            if (pizza == null)
+                return NotFound();
+
+            return pizza;
+        }
+
+    }
+
+    
+}
